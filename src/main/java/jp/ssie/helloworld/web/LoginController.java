a4ad51be21214252e11c9c6f0405790aed2818ef
kaimi <kainni07@gmail.com>
2019-10-09 20:08:39 +0900

    formで画面の値を受け渡す

--- src/main/java/jp/ssie/helloworld/web/LoginController.java
+++ src/main/java/jp/ssie/helloworld/web/LoginController.java
@@ -1,14 +1,25 @@
 package jp.ssie.helloworld.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import jp.ssie.helloworld.form.LoginForm;
 
 @Controller
 public class LoginController {
 
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String index(Model model) {
+		return "index";
+	}
+
 	@RequestMapping(value="/login", method=RequestMethod.POST)
-	public String login() {
+	public String login(Model model, @ModelAttribute("loginForm") LoginForm loginForm) {
+		model.addAttribute("loginName", loginForm.getLoginName());
+
 		return "top";
 	}
 }
